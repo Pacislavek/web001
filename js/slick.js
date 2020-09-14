25dad4ff7e6646fbfcfecb5c3c553909686b237c
pacislavek <pacislavek@gmail.com>
2020-09-14 09:08:16 +0200

    initial commit

---
+++ js/slick.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.iphone-content').slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 2,
+    arrows: false,
+    rows: 2
+    //variableWidth: true,
+    //adaptiveHeight: true
+  });
+});
